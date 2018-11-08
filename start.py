e7ba45ef01a39951efa96189cc0b4db4f03039ac
shind07 <shind@sfu.ca>
2018-11-08 14:09:25 -0800

    import CORS

--- start.py
+++ start.py
@@ -6,6 +6,7 @@ from builtins import *
 from importlib import import_module
 import os, sys
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from config import app_name, bind_port, tasks_dir, data_dir
 
 
